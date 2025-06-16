4069119616659ece112742a3144e46cc0d47c394
Hir2wa <alainfabricehirwa@gmail.com>
2025-06-16 19:22:00 +0200

    added some description on the types

--- src/funtions.ts
+++ src/funtions.ts
@@ -6,3 +6,10 @@ interface GuitaristInterface {
   age?: number; // here we can make a agen optional for a user  by just putting the ? infront of the age please
   level: numberOrString;
 }
+
+//Literal types
+//help to keep your codes dry  "don't repeat yourself"
+let myName: "Alain";
+myName = "Alain";
+console.log(myName);
+//just here to keep like the constant
