93fe4c0e1f691879567be6e1ebec9e511a1de9bf
Hir2wa <alainfabricehirwa@gmail.com>
2025-06-16 11:43:09 +0200

    started array in typescrip

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,14 @@
 let username = "Alain";
+let a = 4;
+let b = 5;
+let c = "a";
 console.log(username);
+
+const sum = (a: number, b: number) => {
+  return a + b;
+};
+
+//Array
+let stringArray = ["a", "b", "c"];
+let guitars = ["stract", "les Paul", 912];
+let mixedDate = ["a", 5, true];
