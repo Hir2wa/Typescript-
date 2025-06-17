0d1b35e2166cfda207929ca00194a9779b53068b
Hir2wa <alainfabricehirwa@gmail.com>
2025-06-17 21:27:54 +0200

    added codewars challenge for typescript

--- build/js/functions.js
+++ build/js/functions.js
@@ -22,3 +22,36 @@ const addAll = (a, b, c) => {
     return a + b;
 };
 //something that could not work default value  won't work when we are working with alias types and interface   method
+const total = (num, ...nums) => {
+    return (num +
+        nums.reduce((prev, current) => {
+            return prev + current;
+        }));
+};
+console.log(total(1, 2, 4, 5, 5, 6));
+//no need to include the starting point it is optional
+//the never type
+const createError = (erroMsg) => {
+    throw new Error(erroMsg);
+};
+const infinite = () => {
+    let i = 1;
+    while (true) {
+        i++;
+        // if(i>100) break
+    }
+};
+const numberOrString = (value) => {
+    if (typeof value === "number")
+        return "string";
+    if (isNumber(value))
+        return "number ";
+    throw new Error("Unreachable ");
+};
+// typscrip needs safty even if you are sure you gone return number or string but needs a safety for  when a error ocurs
+//these never happens on the unreachable code  infinite loops and errros thrown
+//pro tips
+const isNumber = (value) => {
+    return typeof value === "number" ? true : false;
+};
+//worked on introduction to type assertion
