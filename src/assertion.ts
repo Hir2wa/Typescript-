ebd8f4d12e2a0e149411f3cd4b0baf8d57432f87
Hir2wa <alainfabricehirwa@gmail.com>
2025-06-17 15:33:12 +0200

    added typecasting noes

--- src/assertion.ts
+++ src/assertion.ts
@@ -1,2 +1,9 @@
 //type casting is a way of overriding the type of a variable  such as when incorrect  variable is provided by a library
 //in short casting is a way of overriding a variable
+
+let sureName: unknown = "Alain";
+console.log(typeof sureName);
+
+console.log((<string>sureName).length);
+console.log(typeof sureName);
+//from now these sureNane is a string nolonger unkown
