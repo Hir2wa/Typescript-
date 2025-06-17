a8ac11a5df83808adb1b0f589efdf3b80a529d8e
Hir2wa <alainfabricehirwa@gmail.com>
2025-06-17 15:05:12 +0200

    added pro tips for checking anytypes

--- src/functions.ts
+++ src/functions.ts
@@ -63,8 +63,13 @@ const infinite = (): void => {
 
 const numberOrString = (value: number | string): string => {
   if (typeof value === "number") return "string";
-  if (typeof value === "string") return "number ";
+  if (isNumber(value)) return "number ";
   throw new Error("Unreachable ");
 };
 // typscrip needs safty even if you are sure you gone return number or string but needs a safety for  when a error ocurs
 //these never happens on the unreachable code  infinite loops and errros thrown
+
+//pro tips
+const isNumber = (value: any): boolean => {
+  return typeof value === "number" ? true : false;
+};
