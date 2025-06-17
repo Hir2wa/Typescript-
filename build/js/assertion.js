896fed82eb0ff8d944d6d897473182c7a151a1b1
Hir2wa <alainfabricehirwa@gmail.com>
2025-06-17 17:54:59 +0200

    added  missing  compiled file

---
+++ build/js/assertion.js
@@ -0,0 +1,21 @@
+"use strict";
+//type casting is a way of overriding the type of a variable  such as when incorrect  variable is provided by a library
+//in short casting is a way of overriding a variable
+let sureName = "Alain";
+console.log(typeof sureName);
+console.log(sureName.length);
+console.log(typeof sureName);
+let p = "Alain";
+let r = "Alain";
+console.log(r);
+//type alias is like creating you own type  and be reused
+//using the unknown in types casting in double  that where is separate to any type
+const addOrConcat = (a, b, c) => {
+    if (c === "add")
+        return a + b;
+    return "" + a + b;
+};
+console.log(addOrConcat(4, 5, "add"));
+let add = addOrConcat(3, 5, "add");
+console.log(add);
+//
