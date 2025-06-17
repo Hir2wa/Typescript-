842a13137e0244fbd1835281353d145ca9e011d1
Hir2wa <alainfabricehirwa@gmail.com>
2025-06-17 22:46:20 +0200

    removed unwanted fil

--- build/js/assertion.js
+++ build/js/assertion.js
@@ -11,11 +11,9 @@ console.log(r);
 //type alias is like creating you own type  and be reused
 //using the unknown in types casting in double  that where is separate to any type
 const addOrConcat = (a, b, c) => {
-    if (c === "add")
-        return a + b;
-    return "" + a + b;
+  if (c === "add") return a + b;
+  return "" + a + b;
 };
 console.log(addOrConcat(4, 5, "add"));
 let add = addOrConcat(3, 5, "add");
 console.log(add);
-//
