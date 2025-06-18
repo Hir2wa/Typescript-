bbd4de79d5a727fd6ef22a933eb1620d3365e0a4
Hir2wa <alainfabricehirwa@gmail.com>
2025-06-18 15:36:32 +0200

    added classes example

--- src/classes.ts
+++ src/classes.ts
@@ -1,15 +1,25 @@
 //inside class properties and methods are called members
 //visibility modifier
+//protected means  propety can be accessed inside the deriverid class
+//private inside that class only
+
 class Coder {
   constructor(
     public readonly name: string,
     public music: string,
     private age: number,
-    protected lang: string
+    protected lang: string = "Typescript"
   ) {
     this.name = name;
     this.age = age;
     this.lang = lang;
     this.music = music;
   }
+  public getAge() {
+    return `Hello , I'm ${this.age}`;
+  }
 }
+
+let Dave = new Coder("Dave", "Local", 22);
+
+console.log(Dave.getAge);
