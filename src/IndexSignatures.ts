ad586b14af72114b4e7d5c440c506b2f1bd98ade
Hir2wa <alainfabricehirwa@gmail.com>
2025-06-19 15:22:39 +0200

    created interface    assigned it to an obj

--- src/IndexSignatures.ts
+++ src/IndexSignatures.ts
@@ -1,3 +1,20 @@
 //IndexSignature is ts as a topic
 // there are used in the case you are creating and you don't
 //  know actually the number of   keys or properties a user will input
+//when creating object but you don't know exactly names of keys
+//in short you don't know the shape of an object
+
+//typescript asks for index  signature when you try to access the property of
+// object when trying to access the values of object
+
+interface obj {
+  pizza: number;
+  book: number;
+  job: number;
+}
+
+const transaction: obj = {
+  pizza: 80,
+  book: 50,
+  job: 60,
+};
