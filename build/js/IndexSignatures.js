056521fa7e1d32a0028b74b0480865e6556f9a05
Hir2wa <alainfabricehirwa@gmail.com>
2025-06-19 19:13:08 +0200

    added compiled files

--- build/js/IndexSignatures.js
+++ build/js/IndexSignatures.js
@@ -1,1 +1,41 @@
 "use strict";
+//IndexSignature is ts as a topic
+// there are used in the case you are creating and you don't
+//  know actually the number of   keys or properties a user will input
+//when creating object but you don't know exactly names of keys
+//in short you don't know the shape of an object
+const transaction = {
+    pizza: 80,
+    book: 50,
+    job: 60,
+};
+const obje1 = "pizza";
+//accesssing dynmaical tutorial am seiing they are saying it doesn't work actually
+//error saying endex signature  or the tutorial am watching i outdated
+console.log(transaction[obje1]);
+console.log(transaction.job);
+let index = (transactions) => {
+    let index = 0;
+    for (const key in transaction) {
+        index += transactions[key];
+    }
+    return index;
+};
+console.log(index(transaction));
+const student = {
+    name: "Alain",
+    gpa: 56,
+    class: 3000,
+};
+//console.log(student.area);
+//one of the way we canm use in type of assection
+for (const key in student) {
+    console.log(`${key} = ${student[key]}`);
+}
+//these keyaskey  student create a union type of  the  types we have in that obj
+//  so stat i won't raise an error in time wee are iterating
+//here we were referencing  the   interface
+const access = (student, key) => {
+    console.log(`${key}  : ${student[key]}`);
+};
+console.log(access(student, "class"));
