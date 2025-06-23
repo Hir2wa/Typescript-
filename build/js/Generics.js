da8cc75f884576dcccd6ba8bf2b2afe25180f2ea
Hir2wa <alainfabricehirwa@gmail.com>
2025-06-23 16:28:09 +0200

    added  compiled file for generics

--- build/js/Generics.js
+++ build/js/Generics.js
@@ -1,6 +1,13 @@
 "use strict";
-let test = (args) => {
+//starting  with  Generics in typscript  in typsript
+// type gen = (args: string) => string;
+// let test = <T>(args: T): boolean => {
+//   return typeof args === "object" && !Array.isArray(args) && args !== null;
+// };
+// console.log(test(1));
+// console.log(test({ name: "" }));
+//what are mapped types
+const gen = (args) => {
     return typeof args === "object" && !Array.isArray(args) && args !== null;
 };
-console.log(test(1));
-console.log(test({ name: "" }));
+console.log(gen("hello world"));
